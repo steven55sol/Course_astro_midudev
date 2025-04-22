ec496bea22b3c0a4ca6a5f1356ae942456f78058
Arcus <stiven.sol5cinco@gmail.com>
2025-04-22 17:50:25 -0500

    Subiendo la landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField} from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  output: 'server', 
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+        alias: {
+          '@': new URL('./src', import.meta.url).pathname,
+        },
+      }
+  },
+  env:{
+    schema:{
+      SHOW_BUY_BUTTON: envField.boolean({context:'server', access:'public'}),
+      SCORE_API_ENDPOINT: envField.string({context:'server', access:'public'}) 
+    }
+    }
+   
+});
---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const books = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        img: z.string(),
+        readtime: z.number(),
+        description: z.string(),
+        buy: z.object({
+            spain: z.string().url(),
+            usa: z.string().url(),
+        })
+
+    })
+})
+
+export const collections = {books};
